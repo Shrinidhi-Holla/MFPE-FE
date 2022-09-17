c29e36615f9c19efbe0ff5be1f2e7113178ef5ce
Shrinidhi Holla <hollashrinidhi99@gmail.com>
2022-09-17 21:09:31 +0530

    Update app.constants.ts
    changed port

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,7 +1,7 @@
 export const API_URL = "http://localhost:8081"
 export const CN_URL = "http://localhost:8084"
 export const MF_URL = "http://localhost:8083"
-export const SD_URL = "http://localhost:8082"
+export const SD_URL = "http://localhost:8085"
 
 
 
