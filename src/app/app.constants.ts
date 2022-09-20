c0bf9db80fa49932a29a717fea033093fbee45ea
Shrinidhi Holla <hollashrinidhi99@gmail.com>
2022-09-20 22:26:15 +0530

    https

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -25,7 +25,7 @@
 
 
 
-export const API_URL = "http://authorization-env.eba-3gkpzzst.ap-southeast-1.elasticbeanstalk.com"
-export const CN_URL = "http://calculatenetworth2-env.eba-bk99jtwf.ap-southeast-1.elasticbeanstalk.com"
-export const MF_URL = "http://dailymutualfundnav-env.eba-mxz9xmbm.ap-southeast-1.elasticbeanstalk.com"
-export const SD_URL = "http://dailyshareprice-env-1.eba-gya2c4mi.ap-southeast-1.elasticbeanstalk.com"
+export const API_URL = "https://authorization-env.eba-3gkpzzst.ap-southeast-1.elasticbeanstalk.com"
+export const CN_URL = "https://calculatenetworth2-env.eba-bk99jtwf.ap-southeast-1.elasticbeanstalk.com"
+export const MF_URL = "https://dailymutualfundnav-env.eba-mxz9xmbm.ap-southeast-1.elasticbeanstalk.com"
+export const SD_URL = "https://dailyshareprice-env-1.eba-gya2c4mi.ap-southeast-1.elasticbeanstalk.comS"
